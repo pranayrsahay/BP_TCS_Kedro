b6292a3cb4f7a4e109dcc9cd31e3fccd52a5ab92
pranayrsahay <pranay.rsahay@tcs.com>
2024-12-04 23:05:51 +0530

    Update nodes.py

--- Kedro_mnist_Cnn/kedro_mnist_project/src/tcs_kedro_mnist/pipelines/deployment/nodes.py
+++ Kedro_mnist_Cnn/kedro_mnist_project/src/tcs_kedro_mnist/pipelines/deployment/nodes.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 
 # This function predicts the output based on user defined test set Index input. 
 # Optionally, user can define the index in parameters_deployment.yml
-def predict_user_defined(x_test_normalized, best_trained_model, parameters: dict):
+def predict_user_defined(x_test_normalized, y_test, best_trained_model, parameters: dict):
     model = best_trained_model
     #user_defined_index = parameters["index"]
     user_defined_index = int(input("Enter the Index of test set to be predicted:\n"))
@@ -18,5 +18,6 @@ def predict_user_defined(x_test_normalized, best_trained_model, parameters: dict
     prediction = model.predict(x_test_normalized_w_batch)
     predicted_class = np.argmax(prediction)
     print(f"Model output: {predicted_class}")
+    print(f"Actual output : {y_test[user_defined_index]}")
 
 
